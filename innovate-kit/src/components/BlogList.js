535b76955bae46076a6e6417499d75a8d7cb20bc
X0rbit3 <yaintercode@gmail.com>
2024-05-02 00:37:57 +0200

    Added a delete button to each card, removed the initial 'Add' button and implemented a function to handle deleted items\

--- innovate-kit/src/components/BlogList.js
+++ innovate-kit/src/components/BlogList.js
@@ -1,7 +1,7 @@
 // We receive the passed in data using "props" parameter from the parent component and then we can use it in the child component. We can then use the data to display the blog posts in the BlogList component.
 // The "props" parameter is an object that contains all the properties passed to the component. If we want to pass specific data to be received, we can specfy which data we want to access by using the names instead of the whole "props" object itself.
 
-const BlogList = ({blogs, title }) => {
+const BlogList = ({blogs, title, handleDelete }) => {
 
 
     // We use those in case if we are calling the whole props object directly in the component.
@@ -12,14 +12,16 @@ const BlogList = ({blogs, title }) => {
         <div className="blogs-container">
         
             {/* The curly braces are for injecting javascript code like below */}
-            {/* What we are doing below is mapping the array of the blogs and calling the individual items through the mapped array. The "blog" value is the data from the array items as a whole. React requires a key!*/}
+            {/* What we are doing below is mapping the array of the blogs and calling the individual items through the mapped array. The "blog" value is the data from the array items as a whole, it is a name we can choose. React requires a key!*/}
             { blogs.map((blog) => (
-                // The key is a unique value that is used to identify each item in the array.
+                // The key is a unique value that is used to identify each item in the array. Duh!
                 // The key value should be unique for each item in the array. The key value is used by React to keep track of the items in the array and to update the items in the array efficiently.
                 <div className="blogs-items">
                     <div className="blog-card" key={blog.id}>
                         <h2>{ blog.title }</h2>
-                        <p>Written by { blog.author }</p>
+                        <p>Written by {blog.author}</p>
+                        {/* We are firing an anonymous function here to execute the handleDelete function */}
+                        <button className='del-btn' onClick={() => handleDelete(blog.id)}>Delete</button>
                     </div>
                 </div>
             ))}
--- innovate-kit/src/components/Home.js
+++ innovate-kit/src/components/Home.js
@@ -14,8 +14,15 @@ const Home = () => {
         { title: 'My new job', body: 'I have a new job at Innovate Kit!', author: 'Naomi', id: 6 }
     ]);
 
-    const handleClick = () => { 
+    // If a blog post is deleted, we can use the handleDelete function to delete the blog post. We can use the filter method to filter out the blog post that we want to delete. The id is passed in as an argument to the handleDelete function. 
+    // And if the id of the blog post matches the id of the blog post that we want to delete, we can filter out the blog post that we want to delete.
+    const handleDelete = (id) => { 
+        // We can use the filter method to filter out the items that we want to keep. The filter method returns a new array with the items that pass the condition. The filter method does not change the original array. It doesn't mutate the original array.
+        // We are filtering out the blog items with each "blog" that do not have the same id as the one we are deleting. We are then setting the new array to the setBlogs function to update the state value.
+        const newBlogs = blogs.filter(blog => blog.id !== id);
 
+        // We are updating the state value of the blogs array with the newBlogs array. We are setting the newBlogs array to the setBlogs function to update the state value.
+        setBlogs(newBlogs);
     }
 
     return ( 
@@ -25,9 +32,10 @@ const Home = () => {
             {/* To use the data within the BlogList component, we can use props to be able to easily manipulate them and make the data more reusable. Props is a way to pass data from one component to another or child component*/}
             {/* We can pass in a title with "title='All Blogs'" by just adding this behind "{blogs}"*/}
             {/* The "blogs=" property can be called whatever, the value is a dynamic value which is the actual blogs prop from the useState function */}
-            <BlogList blogs={blogs} /> 
+            <BlogList blogs={blogs} handleDelete={ handleDelete } /> 
+            {/* We can reuse the prop like below */}
+            {/* <BlogList blogs={blogs.filter((blog)=> blog.author === 'Yaz')} title="Test"/>  */}
 
-            <button className='add-btn' onClick={handleClick}>Add</button>
         </div>
     );
 }
