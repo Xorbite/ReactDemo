73b0f0f510b7dfd3ce2a764f7d486470bfa06d70
X0rbit3 <yaintercode@gmail.com>
2024-05-03 14:07:48 +0200

    Added form styling

--- innovate-kit/src/components/Create.js
+++ innovate-kit/src/components/Create.js
@@ -3,21 +3,27 @@ const Create = () => {
         <div className="create">
             <h2>Add a New Blog</h2>
             <form>
-                <label>Blog Title:</label>
-                <input
-                    type="text"
-                    required
-                />
-                <label>Blog Body:</label>
-                <textarea
-                    required
-                ></textarea>
+                <div className="form-items">
+                    <label>Blog Title:</label>
+                    <input
+                        type="text"
+                        required
+                    />
+                </div>
+                <div className="form-items">
+                    <label>Blog Body:</label>
+                    <textarea
+                        required
+                    ></textarea>
+                </div>                
+                <div className="form-items">
                 <label>Blog Author:</label>
-                <select>
-                    <option value="Yaz">Yaz</option>
-                    <option value="Yaz">Yaz</option>
-                    <option value="Yaz">Yaz</option>
-                </select>
+                    <select>
+                        <option value="Yaz">Yaz</option>
+                        <option value="Yaz">Yaz</option>
+                        <option value="Yaz">Yaz</option>
+                    </select>
+                </div>   
                 <button>Add Blog</button>
             </form>    
         </div>
